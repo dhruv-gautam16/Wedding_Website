48eb94585037c3b8efe2ce2ababd859ae6cab66a
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-04-24 01:35:33 +0530

    Added all the paths to 5 pages

--- wedding_website/app.js
+++ wedding_website/app.js
@@ -6,6 +6,22 @@ app.get('/',(req,res)=>{
     res.render('home');
 })
 
+app.get('/first_page',(req,res)=>{
+    res.render('first_page');
+})
+
+app.get('/second_page',(req,res)=>{
+    res.render('second_page');
+})
+
+app.get('/third_page',(req,res)=>{
+    res.render('third_page');
+})
+
+app.get('/fourth_page',(req,res)=>{
+    res.render('fourth_page');
+})
+
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, 'views'));
 
